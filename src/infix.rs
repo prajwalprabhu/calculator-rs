02d1d08355f18300310155068cbea92b82e424bc
T Prajwal Prabhu <prajwalprabhu.tellar@gmail.com>
2021-03-09 05:46:33 +0530

    small chnages

--- src/infix.rs
+++ src/infix.rs
@@ -1,37 +1,56 @@
 //Solve Post fix expression
-pub fn eval(eq: String) -> u32 {
-    check(eq.clone());
-    to_result(to_post(power(to_vec(eq))))
+pub fn eval(eq: String) -> Result<i32,String> {
+    // check(eq.clone());
+    to_result(to_post(to_vec(eq)))
 }
-fn check(eq: String) {
-    let eq = eq.clone();
-    let mut count = 0;
-    for i in eq.chars() {
-        if i == '(' {
-            count += 1;
-        }
-        if i == ')' {
-            count -= 1;
-        }
-    }
-    if count > 0 {
-        panic!("Braces opened but not closed");
-    }
-    if count<0{
-		panic!("Extra closing Braces");
-	}
-}
-fn to_result(result: Vec<String>) -> u32 {
-    let mut stack: Vec<u32> = Vec::new(); //Stack
+// fn check(eq: String) {
+//     let eq = eq.clone();
+//     let mut count = 0;
+//     for i in eq.chars() {
+//         if i == '(' {
+//             count += 1;
+//         }
+//         if i == ')' {
+//             count -= 1;
+//         }
+//     }
+//     if count > 0 {
+//         panic!("Braces opened but not closed");
+//     }
+//     if count<0{
+// 		panic!("Extra closing Braces");
+// 	}
+// }
+fn to_result(result: Vec<String>) -> Result<i32,String> {
+    let mut stack: Vec<i32> = Vec::new(); //Stack
+    // let mut operand: Vec<i32>
     let operator = "*/+-".to_string();
     for i in result {
         if i != "" {
             if operator.contains(&i) {
-                let a = stack.pop().unwrap();
-                let b = stack.pop().unwrap();
+                let aa = stack.pop();
+                let bb = stack.pop();
+                let mut a = 0;
+                let mut b = 0;
+                if aa.is_some(){
+                    let _a =&mut a;
+                    *_a= aa.unwrap();
+                }else{
+                    return Err("Syntax Error".to_string());
+                }
+                if bb.is_some(){
+                    let _b =&mut b;
+                    *_b = bb.unwrap();
+                }else{
+                    return Err("Syntax Error".to_string());
+                }
                 if i == "+" {
                     stack.push(b + a);
                 } else if i == "-" {
+                    // if a>b{
+                    //     let ris = (a-b);
+                    //     stack.push(ris)
+                    // }else{
                     stack.push(b - a);
                 } else if i == "*" {
                     stack.push(b * a);
@@ -43,64 +62,64 @@ fn to_result(result: Vec<String>) -> u32 {
             }
         }
     }
-    stack.pop().unwrap()
+    Ok(stack.pop().unwrap())
 }
 
 //Checks whether there is any power equation in given equation i.e 2^3=8 or (1+1)^3=8
-fn power(mut eq: Vec<String>) -> Vec<String> {
-    // //println!("eq = {:?}", eq);
-    let result: u32;
-    let a: String;
-    let b: String;
-    let mut eq2: Vec<String> = Vec::new();
-    if eq.contains(&'^'.to_string()) {
-        for i in 0..eq.len() - 1 {
-            if eq[i] == '^'.to_string() {
-                a = eq[i - 1].clone();
-                b = eq[i + 1].clone();
-                if a == ')'.to_string() {
-                    let mut j = i - 2;
-                    let mut open = 0;
-                    eq.remove(i - 1);
-                    while j > 0 {
-                        if eq[j] == ')'.to_string() {
-                            open += 1;
-                        }
-                        if eq[j] == '('.to_string() {
-                            if open > 0 {
-                                open -= 1;
-                            } else {
-                                eq.remove(j);
-                                break;
-                            }
-                        }
-                        eq2.insert(0, eq.remove(j));
-                        j -= 1;
-                    }
-                    let res2 = to_result(to_post(power(eq2.clone())));
-                    let res2 = format!("{}", res2);
-                    eq.insert(j, res2);
-                    break;
-                } else {
-                    let a: u32 = a.parse().unwrap();
-                    let b: u32 = b.parse().unwrap();
-                    result = a.pow(b);
-                    let result = format!("{}", result);
-                    eq.remove(i - 1);
-                    eq.remove(i - 1);
-                    eq.remove(i - 1);
-                    eq.insert(i - 1, result);
-                    break;
-                }
-            }
-        }
-    }
-    if eq.contains(&'^'.to_string()) {
-        return power(eq);
-    }
-    // to_result(to_post(eq))
-    eq
-}
+// fn power(mut eq: Vec<String>) -> Vec<String> {
+//     // //println!("eq = {:?}", eq);
+//     let result: i32;
+//     let a: String;
+//     let b: String;
+//     let mut eq2: Vec<String> = Vec::new();
+//     if eq.contains(&'^'.to_string()) {
+//         for i in 0..eq.len() - 1 {
+//             if eq[i] == '^'.to_string() {
+//                 a = eq[i - 1].clone();
+//                 b = eq[i + 1].clone();
+//                 if a == ')'.to_string() {
+//                     let mut j = i - 2;
+//                     let mut open = 0;
+//                     eq.remove(i - 1);
+//                     while j > 0 {
+//                         if eq[j] == ')'.to_string() {
+//                             open += 1;
+//                         }
+//                         if eq[j] == '('.to_string() {
+//                             if open > 0 {
+//                                 open -= 1;
+//                             } else {
+//                                 eq.remove(j);
+//                                 break;
+//                             }
+//                         }
+//                         eq2.insert(0, eq.remove(j));
+//                         j -= 1;
+//                     }
+//                     let res2 = to_result(to_post(power(eq2.clone())));
+//                     let res2 = format!("{}", res2);
+//                     eq.insert(j, res2);
+//                     break;
+//                 } else {
+//                     let a: i32 = a.parse().unwrap();
+//                     let b: i32 = b.parse().unwrap();
+//                     result = a.pow(b);
+//                     let result = format!("{}", result);
+//                     eq.remove(i - 1);
+//                     eq.remove(i - 1);
+//                     eq.remove(i - 1);
+//                     eq.insert(i - 1, result);
+//                     break;
+//                 }
+//             }
+//         }
+//     }
+//     if eq.contains(&'^'.to_string()) {
+//         return power(eq);
+//     }
+//     // to_result(to_post(eq))
+//     eq
+// }
 
 fn to_vec(eq: String) -> Vec<String> {
     let mut operand = String::new();
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,16 @@
-// To import all needed traits.
 use gio::prelude::*;
 use gtk::prelude::*;
 use std::env;
-// mod layout;
 mod infix;
 
 fn main() {
     let uiapp = gtk::Application::new(
-        Some("org.gtkrsnotes.demo"),
+        Some("github.prajwalprabhu.calculator-rs"),
         gio::ApplicationFlags::FLAGS_NONE,
     )
     .expect("Application::new failed");
     uiapp.connect_activate(|app| {
         let win = gtk::ApplicationWindow::new(app);
-        // let layout = include!("layout.rs");
-        // let layout =layout::init();
         let layout = run();
         win.add(&layout);
         win.show_all();
@@ -24,6 +20,7 @@ fn main() {
 }
 
 fn run()->gtk::Box{
+    let history:Vec<String> = Vec::new();
     let main_box = gtk::BoxBuilder::new()
         .homogeneous(true)
         .spacing(10)
@@ -43,7 +40,7 @@ fn run()->gtk::Box{
     }
     
 let value = [["sqrt","7","8","9","/"],
-["+/-","4","5","6","*"],
+["C","4","5","6","*"],
 ["AC","1","2","3","-"],
 ["0","00",".","=","+"]];
 for (i,val) in boxes.iter().enumerate() {
@@ -53,8 +50,18 @@ for (i,val) in boxes.iter().enumerate() {
         b.connect_clicked(move |_| {
             let mut old =screen_.get_text().to_string();
             if j== "=" {
-                old = format!("{}", infix::eval(old.clone()));
-            } else {
+                let res = infix::eval(old.clone());
+                match res {
+                    Ok(ans)=>old = format!("{}",ans),
+                    Err(e) =>old=e
+                }
+
+            }else if j=="AC" || j=="C"{
+                old = "".to_string();
+            } else if j=="sqrt" {
+                let ris:f32=old.parse().unwrap();
+                old= format!("{}", ris.sqrt());
+            }else {
                 old.push_str(j);
             }
             screen_.set_text(&old);
